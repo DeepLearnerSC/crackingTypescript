5acdd2311587bc3076ebe9fca7b80147b382c70e
Song Cho <deeplearner507@gmail.com>
2019-08-29 23:24:46 +0900

    inital commit
    react-ts-bable setup part 1
    
    pre setup done!
    
    change dirname

---
+++ example/create-react-app/src/App.tsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import { number } from 'prop-types';
+
+class App extends Component {
+
+  sum(a:number, b:number):number {
+    return a + b
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.sum(3,2)}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
