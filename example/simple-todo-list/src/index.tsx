08b188346aa3aa10c20f85501ba394157275d982
Song Cho <deeplearner507@gmail.com>
2019-08-30 23:52:33 +0900

    todo-list project done

--- example/simple-todo-list/src/index.tsx
+++ example/simple-todo-list/src/index.tsx
@@ -1,15 +1,71 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React, { Fragment, useState, useCallback } from "react";
+import ReactDom from "react-dom";
+import { number } from "prop-types";
 
-export default function App():JSX.Element {
-    const sum =(a: number, b: number):number => a + b
-    return (
-        <h1>
-           Hello {sum(10,8)}
-        </h1>
-    )
+type FormElem = React.FormEvent<HTMLFormElement>;
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    console.log(newTodos);
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <Fragment>
+      <h1>Todo-list</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <Fragment key={index}  >
+            <div
+              style={{ textDecoration: todo.complete ? "line-through" : "" }}
+            >
+              {todo.text}
+            </div>
+            <button type="button" onClick={() => completeTodo(index)}>
+              {" "}
+              {todo.complete ? "Incomplete" : "complete"}{" "}
+            </button>
+            <button type="button" onClick={() => removeTodo(index)}>&times;</button>
+          </Fragment>
+        ))}
+      </section>
+    </Fragment>
+  );
 }
 
-const root = document.getElementById('app-root')
+const root = document.getElementById("app-root");
 
-ReactDom.render(<App/>, root)
+ReactDom.render(<App />, root);
