dbb70c5c46618da3e023b330ab236d38dc31a4e9
Song Cho <deeplearner507@gmail.com>
2020-01-09 00:39:31 +0900

    typescript basic types

---
+++ Types/README.md
@@ -0,0 +1,76 @@
+TypeScripts adds ...
+1. Types
+2. Next-gen JavaScript Features (compiled down for older Browesers)
+3. Non-javaScript Featrues like Interfaces or Generics
+4. Meta-Programming features like Decorators.
+5. Rich Configuration Options
+6. Morden Tooling that helps even in non-Typescript Projects.
+
+
+npm install
+
+
+How to complie the ts file
+
+tsc app.ts
+
+how to execute the client server
+
+npm start
+
+developer tools
+see the console log and test your understanding.
+
+
+
+
+Working with Types
+1. number 
+: All numbers, no differentiation between integers or floats.
+ex) 1, 5.3, -10
+2. string 
+: All text values
+ex) 'Hi', "Bye", `LOL`
+3. boolean
+: Just these two, no "truthy" or "falsy" values
+ex) true false
+4. object
+: Any JavaScript object, more specific types (type of object) are possible.
+ex) {year: 2020}
+5. array
+: Any JavaScript array, type can be flexible or strict (regarding the element types)
+6. tuple
+: Added by TypeScript: Fixed-length array
+ex) [1,2]
+Notice! TypeScript's **type system only helps you during developmment** (before the code gets compiled.)
+7. enum 
+: Added by TypeScript: Automatically enumerated global constant identifiers.
+ex) enum { NEW, OLD }
+8. any
+: Any kind of value, no specific type assignment
+ex) *
+
+JavaScript is dynamically typed which means it's perfectly fine that we have a variable which initially
+might hold a number where we later assign a string to it.
+And that's why we have the type of operator so that we can check the current type of something at runtime
+if we have some code that depends on a certain type 
+Typescript on the other end is statically type which means we define the types of variables 
+and parameters ends on during development. They don't suddenly change during runtime.
+
+Now of course since typescript is compiled to JavaScript they theoretically could.
+But if we use typescript and we write code where we suddenly assign a new type of data into a variable
+(where we previously set that this should be a number for example.
+And now we're assigning a string then we get an error during development.)
+So we are forced to be clear regarding the types something can or can not hold.
+That's the difference here.
+
+The key difference is: JavaScript uses "dynamic types" (resolved at runtime),
+TypeScript uses "static types" (set during development).
+
+
+app.ts에 있는 코드를 참조 바람.
+
+
+
+Why are "Types" useful and offer an advantage compare to vanilla JavaScript?
+Types allow you to detect if data of a wrong type is used or pass around, hence you can avoid unexpected runtime errors.
--- Types/app.js
+++ Types/app.js
@@ -1,89 +1,127 @@
-"use strict";
-// string
-var myName = 'Max';
-// myName = 28;
-// number
-var myAge = 27;
-// myAge = 'Max';
-// boolean
-var hasHobbies = false;
-// hasHobbies = 1;
-// assign types
+console.log("Chapter 1: TypeScript Basics ");
+/////////////////////////////////////////////////////////////////////////////
+// Type 1: number, boolean, string
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 20;
+var number2 = 20; // '20' => Argument of type '"20"' is not assignable to parameter of type 'number'.
+var printResult = true;
+var resultPhrase = "Result is: ";
+var addResult = add(number1, number2, printResult, resultPhrase);
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+// Type 2 : assign types, array, tuples, any
 var myRealAge;
 myRealAge = 27;
-// myRealAge = '27';
-// array
+// myRealAge = '27'; => error!
 var hobbies = ["Cooking", "Sports"];
-hobbies = [100];
-// hobbies = 100;
-// tuples
+// hobbies = [100];
+// hobbies = 100; => error!
+for (var _i = 0, hobbies_1 = hobbies; _i < hobbies_1.length; _i++) {
+    var hobby = hobbies_1[_i];
+    console.log(hobby.toUpperCase());
+}
 var address = ["Superstreet", 99];
-// enum
+// address.push('is this possible?, unfortunately yes...')
+// address[1] = 'not possible' => error
+// address = ['wow, this workS!', 77]
+// address = ['nope', 1004, 'not a chance'] => error
 var Color;
 (function (Color) {
     Color[Color["Gray"] = 0] = "Gray";
     Color[Color["Green"] = 100] = "Green";
-    Color[Color["Blue"] = 2] = "Blue"; // 2
+    Color[Color["Blue"] = 101] = "Blue"; // it is 101 !!!! not 2!
 })(Color || (Color = {}));
 var myColor = Color.Blue;
-console.log(myColor);
-// any
+console.log("this is enum, and my favorite number is ", myColor);
+// any => not recommended. no point of using it.
 var car = "BMW";
 console.log(car);
 car = { brand: "BMW", series: 3 };
 console.log(car);
-// functions
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+// Type 3: object, nested object
+var userData = {
+    name: "Song",
+    age: 31
+};
+console.log("My name is", userData.name, ".");
+var nested = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+function combine(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    // if (resultConversion === 'as-number') {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
+}
+var combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
+// Type 5: functions
+function sum(n1, n2) {
+    return n1 + n2;
+}
+// return type is void
+function printSum(num) {
+    console.log('Result: ' + num);
+}
 function returnMyName() {
-    return myName;
+    return "Song";
 }
 console.log(returnMyName());
-// void
-function sayHello() {
-    console.log("Hello!");
+var combineValues;
+combineValues = sum;
+// combineValues = printSum; // will cause an error!
+// combineValues = 5;
+console.log(".....", combineValues(8, 8));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
 }
-// argument types
-function multiply(value1, value2) {
-    return value1 * value2;
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+    return 'what?';
+    //callback functions can return something, even if the argument on which
+    // they're passed odes NOT expect a returned value.
+});
+// unknown types
+var userInput; // need extra type check!!
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-// console.log(multiply(2, 'Max'));
-console.log(multiply(10, 2));
-// function types
-var myMultiply;
-// myMultiply = sayHello;
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(5, 2));
-// objects
-var userData = {
-    name: "Max",
-    age: 27
-};
-// userData = {
-//     a: "Hello",
-//     b: 22
-// };
-// complex object
-var complex = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
-    }
-};
-var complex2 = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
-    }
-};
-// union types
-var myRealRealAge = 27;
-myRealRealAge = "27";
-// myRealRealAge = true;
-// check types
-var finalValue = 30;
-if (typeof finalValue == "number") {
-    console.log("Final value is a number");
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
 }
+generateError('An error occurred!', 500);
 var bankAccount = {
     money: 2000,
     deposit: function (value) {
