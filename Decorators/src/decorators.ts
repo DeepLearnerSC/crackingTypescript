1f42ec7e0e7829e61a0edbf7f41169e6fed14669
Song Cho <deeplearner507@gmail.com>
2020-01-31 23:08:39 +0900

    creating autoBind decorator

--- Decorators/src/decorators.ts
+++ Decorators/src/decorators.ts
@@ -1,6 +1,6 @@
 // Basic Decorator and Decorator Factories
-function Logger(logString:string) {
-    console.log('LOGGER FACTORY');
+function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
   return function(constructor: Function) {
     console.log("Logging...", logString);
     console.log(constructor);
@@ -9,20 +9,26 @@ function Logger(logString:string) {
 
 function WithTemplate(template: string, hookId: string) {
     console.log('TEMPLATE FACTORY');
-    return function(constructor: any) {
-        console.log('Rendering template')
-      const hookEl = document.getElementById(hookId);
-      const p = new constructor();
-      if (hookEl) {
-        hookEl.innerHTML = template;
-        hookEl.querySelector('h1')!.textContent = p.name;
-      }
-    }
-}
+    return function<T extends { new (...args: any[]): {name: string} }>(
+      originalConstructor: T
+    ) {
+      return class extends originalConstructor {
+        constructor(..._: any[]) {
+          super();
+          console.log('Rendering template');
+          const hookEl = document.getElementById(hookId);
+          if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = this.name;
+          }
+        }
+      };
+    };
+  }
 
 // Adding mulitple decorators Possible!
-@Logger('LOGGING - PERSON')
-@WithTemplate('<h1>My Super Normal Web</h1>','app')
+@Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Super Normal Web</h1>", "app")
 class Person {
   name = "Shaman";
 
@@ -31,8 +37,95 @@ class Person {
   }
 }
 
-const pers = new Person();
+// const pers = new Person();
 
-console.log(pers);
+// console.log(pers);
 /////////////////////////////////////////////////////////////////
 
+///////////////////Property Decorator////////////////////////////
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+  function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+  }
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29);
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+// p.showMessage();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
